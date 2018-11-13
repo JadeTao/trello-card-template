91c0fd250fdab499d5b7d67a839bda6cdb8c4559
jader_tao <jader_tao@qq.com>
2018-11-13 22:56:32 +0800

    init commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+# trello 卡片助手
+快速创建预设好的某种卡片，仅适用于 Google Chrome 浏览器。
+
+# 安装
+1. 访问 [谷歌应用商店](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)，下载 tampermonkey。
+2. 点击浏览器旁的 tampermonkey 图标，点击添加新脚本。
+3. 将 `template.js` 内容复制到新页面中，使用组合键 `command + s 进行保存
+
+# 使用
+1. 新建一个卡片后，单击该卡片，进入卡片详情模态框。
+2. 在内容编辑区输入 `[TYPE]`，点击保存，即可生成预设好的模板。目前，`TYPE` 可以使用的值为 `userstory`,`bug`,`child`
+
+# 注意事项
+- 下载 Google Chrome 插件时可能需要科学上网。
+- 目前该功能不支持为已有内容的卡片新增内容。
+- 内容可继续丰富，支持自定义。
+- 如失效，请刷新浏览器。
---
+++ template.js
@@ -0,0 +1,101 @@
+// ==UserScript==
+// @name         trello enhancement for ruff
+// @namespace    ruff
+// @version      0.1
+// @description  description scaffold
+// @author       jader tao
+// @grant        none
+// @include      https://trello.com/*
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  var store = {
+      '[userstory]':`# 背景故事
+1. <内容>
+2. <内容>
+3. <内容>
+
+# 用户价值
+1. <内容>
+2. <内容>
+3. <内容>
+
+# 业务内容
+1. <内容>
+2. <内容>
+3. <内容>
+
+# AC
+1. <验收标准>
+2. <验收标准>
+3. <验收标准>
+
+# 参考资料
+<参考资料>
+`, '[bug]':`## 描述
+<bug描述>
+
+## 环境
+[√] test
+[×] staging
+[×] prod
+
+## 用户
+<账户信息或其他内容>
+
+## 复现
+1. <复现操作>
+2. <复现操作>
+3. <复现操作>
+
+## 预期
+<预期可作为AC>
+`,'[child]':`## 母卡
+<母卡链接>
+
+## 方案
+1. <内容>
+2. <内容>
+3. <内容>
+
+## AC
+1. <验收标准>
+2. <验收标准>
+3. <验收标准>
+`
+  }
+  window.__handler = setInterval(function() {
+      var path = window.location.pathname;
+      if (path.startsWith('/b/')) {
+          var a = document.querySelectorAll('a.list-card.js-member-droppable');
+          if (a && a.length !== 0) {
+              clearInterval(window.__handler);
+              Array.from(a).forEach(node=>node.addEventListener('click', function() {
+                  var textarea = document.querySelector('textarea.field.js-description-draft.card-back-description.autosave');
+
+                  if (textarea) {
+                      textarea.addEventListener('blur', function() {
+                          var trimmedValue = String(textarea.value).trim();
+                          if (store.hasOwnProperty(trimmedValue)) {
+                              textarea.value = store[trimmedValue];
+                          }
+                      })
+                  }
+              }))
+          }
+      } else if (path.startsWith('/c/')) {
+          var textarea = document.querySelector('textarea.field.js-description-draft.card-back-description.autosave');
+          if (textarea) {
+              clearInterval(window.__handler);
+              textarea.addEventListener('blur', function() {
+                var trimmedValue = String(textarea.value).trim();
+                if (store.hasOwnProperty(trimmedValue)) {
+                    textarea.value = store[trimmedValue];
+                }
+              })
+          }
+      }
+  }, 300)
+}
+)(window);
